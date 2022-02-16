856a433c6f7cd04d25b84db35e91a3c0125777dc
Ghiles Mekdam <ghilesmekdem@gmail.com>
2022-02-16 22:42:32 +0100

    orders graph data working

--- controllers/getGraphData.js
+++ controllers/getGraphData.js
@@ -14,11 +14,30 @@ let getGraphData= (req,resp)=>{
         try{
             jwt.verify(token,'admin')
             let ordersData=[]
-            for(let i=1; i < 12; i++){
-                let startDay= fns.format(new Date(01,i,Number(year)), "dd-mm-yyyy")
-                let endDay= fns.lastDayOfMonth(startDay)
-               console.log(startDay, endDay)
+            for(let i=0; i < 12; i++){
+
+                // let startDay= fns.format(new Date(01,i,Number(year)), "dd-mm-yyyy")
+                // let endDay= fns.lastDayOfMonth(new Date(startDay))
+                // let startDay= fns.format(new Date(2022 ,4, 08), "dd/MM/yyyy")
+                let startDay= fns.format(new Date(2022, i, 01), "yyyy/MM/dd")
+                let endDay= fns.format(fns.lastDayOfMonth(new Date(startDay)), "yyyy/MM/dd")
+               console.log(startDay , startDay instanceof Date, endDay)
+               orders.find({date: { $gte: startDay , $lte: endDay}},(err, result)=>{
+                   if(err!=null){
+                       console.log(err)
+                   }else{
+                       let ordersNumber= result.length
+                       ordersData.push(ordersNumber)
+                       console.log(ordersNumber, ordersData)
+                       if(i==11){
+                        console.log(ordersData)
+                        resp.status(200).json({ ordersByMonth: ordersData})
+                       }
+                    }
+               })
+               
             }
+           
             // let  januaryOrders, februaryOrders, marchOrders, aprilOrders, mayOrders, juneOrders,julyOrders, augustOrders, septemberOrders, octoberOrders,novemberOrders,decemberOrders
             // let monthlyOrders= [ januaryOrders, februaryOrders, marchOrders, aprilOrders, mayOrders, juneOrders,julyOrders, augustOrders, septemberOrders, octoberOrders,novemberOrders,decemberOrders]
             // monthlyOrders.map((el,idx)=>{
